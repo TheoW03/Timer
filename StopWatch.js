528564bee7b1dfa054f70aa67cc705d31017bf25
Theo <95878514+TheoW03@users.noreply.github.com>
2022-03-18 11:02:03 -0400

    Added more JS. worked on GUI features

--- StopWatch.js
+++ StopWatch.js
@@ -4,13 +4,28 @@ var mins = 0;
 var hours = 0;
 let paused=true;
 var Lap="";
+var pressedLap=false;
 
 function start(){
+    if(pressedLap){
+        let c = document.getElementById('timer');
+        c.innerHTML="00:00:00:00"
+        miliSeconds=0;
+        secs=0;
+        mins=0;
+        hours=0;
+        pressedLap=false;
+    }
     paused=false;
 }
 function stop(){
     paused=true;
 }
+/**
+ * 
+ * @returns returns nothing
+ * 
+ */
 function stopWatch(){  
     if(!paused){
         if(miliSeconds == 10){
@@ -23,21 +38,22 @@ function stopWatch(){
         }
         let c = document.getElementById('timer');
 
-        c.innerHTML=  mins.toString() +": 0" + secs.toString() +": 0" + miliSeconds.toString();
-        Lap = mins.toString() +": 0" + secs.toString() +": 0" + miliSeconds.toString();
+        c.innerHTML=  mins.toString() +":0" + secs.toString() +":0" + miliSeconds.toString();
+        Lap = mins.toString() +":0" + secs.toString() +":0" + miliSeconds.toString();
         miliSeconds++;    //will update
     }else{
         return;
     }
 }
 /**
- * this is lab 
+ * this is lap
  * code later
  */
 var id = setInterval(stopWatch,100);
 function lap(){
     let c = document.getElementById('laps');
     c.innerHTML = Lap;
+    pressedLap=true;
     stop();
 }
 
