df528c6277f3f76c15c04f19893755523dfef15f
Theo <95878514+TheoW03@users.noreply.github.com>
2022-03-25 09:50:13 -0400

    added local time

--- WorldClockScript.js
+++ WorldClockScript.js
@@ -1,3 +1,17 @@
-function getTime(){
-    
+
+window.onload = function () {
+    let daysOfweek=['friday','saturday','sunday','monday','tuesday','wedsenday','thursday'];
+    let c = document.getElementById("test");
+    let today = new Date();
+    let dd = String(today.getDate()).padStart(2, '0');
+    let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    let yyyy = today.getFullYear();
+    let c2=document.getElementById('positive');
+    let t =  new Date(mm,dd ,yyyy);
+    c.innerHTML = "today date: " + mm + '/' + dd + '/' + yyyy;
+    c2.innerHTML = "we hope your having a good " + daysOfweek[t.getDay()-1] + "!";
+
+}
+function getTime() {
+
 }
