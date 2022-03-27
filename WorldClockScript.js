db72b02837f8277e177d897ce7fac85d57a2a7ca
Theo <95878514+TheoW03@users.noreply.github.com>
2022-03-27 13:58:51 -0400

    fixed getDay

--- WorldClockScript.js
+++ WorldClockScript.js
@@ -1,7 +1,9 @@
 var local = new Date().toLocaleTimeString();
 
+
 window.onload = function () {
-    let daysOfweek = ['Friday', 'Saturday', 'Sunday', 'Monday', 'Tuesday', 'Wedsenday', 'Thursday'];
+    // var day = localStorage.getItem('days');
+    let daysOfweek = ['Sunday', 'Monday', 'Tuesday', 'Wedsenday', 'Thursday', 'Friday', 'Saturday'];
     let c = document.getElementById("test");
     let today = new Date();
     let dd = String(today.getDate()).padStart(2, '0');
@@ -9,26 +11,37 @@ window.onload = function () {
     let yyyy = today.getFullYear();
     let c2 = document.getElementById('positive');
     let c3 = document.getElementById('localTime');
-    let t = new Date(mm, dd, yyyy);
+    let t = new Date(mm+"-"+dd+"-"+yyyy);
+
+
     c.innerHTML = "today date: " + mm + '/' + dd + '/' + yyyy;
     c2.innerHTML = "we hope your having a good " + daysOfweek[t.getDay()] + "!";
+
     setInterval(getTime, 1000);
     let me = document.getElementById('mes');
     if (local.includes("AM")) {
         me.innerHTML = "Good morning";
-    }else if(local.includes('PM')){
+    } else if (local.includes('PM')) {
         me.innerHTML = "Have a good evening/afternoon";
     }
     //else if(Integer.parseInt(local.charAt(0)) == 12){
     //     me.innerHTML = "Good its noon. enjoy your lunch/tea";
-    
+
     // }else if(Integer.parseInt(local.charAt(0)) >= 1 && Integer.parseInt(local.charAt(0)) <= 5 ){
     //     me.innerHTML = "good afternoon";
     // } 
     // else {
     //     me.innerHTML = "good evening";
     // }
-    c3.innerHTML = "Clock: " + local;
+
+    // c3.innerHTML = "Clock: " + local;
+    // if (local == "1:37:40 PM") {
+    //     day++;
+    //     if (day == 7) {
+    //         day = 0;
+    //     }
+    //     localStorage.setItem('days', 0);
+    // }
 
 
 }
@@ -37,12 +50,12 @@ function getTime() {
     let c3 = document.getElementById('localTime');
     if (local.includes("AM")) {
         me.innerHTML = "Good morning";
-    }else if(local.includes('PM')){
+    } else if (local.includes('PM')) {
         me.innerHTML = "Have a good evening/afternoon";
-    } 
+    }
     //else if(Integer.parseInt(local.charAt(0)) == 12){
     //     me.innerHTML = "Good its noon. enjoy your lunch/tea";
-    
+
     // }else if(Integer.parseInt(local.charAt(0)) >= 1 && Integer.parseInt(local.charAt(0)) <= 5 ){
     //     me.innerHTML = "good afternoon";
     // } else {
