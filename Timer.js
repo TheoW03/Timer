21fc1beb8fe83562ec9b91171eb707a90ee60104
Theo <95878514+TheoW03@users.noreply.github.com>
2022-03-28 10:04:15 -0400

    added more formating.

--- Timer.js
+++ Timer.js
@@ -1,5 +1,20 @@
  // import {timerr} from "./TimerClass.js";
+//  function showNotification() {
+//     const notification = new Notification("New message incoming", {
+//        body: "your timer is up!!!"    
+//     });
+//     alert("called")
+//  }
+//  window.onload = function(){
+//     if (Notification.permission !== "denied") {
+//         alert("turn notifications on");
+//         Notification.requestPermission().then(permission => {
 
+//         });
+//     }
+//     showNotification();
+
+// }
 var paused = true;
 var secs;
 var hours;
@@ -27,15 +42,15 @@ function startTimers() {
         minsId = document.getElementById("mins");
         secsId = document.getElementById("seconds");
         hours = hoursId.value;
-        if(hours == null){
+        if(hours == null || hours < 0){
             hours = 0;
         }
         mins = minsId.value;
-        if(mins == null){
+        if(mins == null || mins < 0){
             mins = 0;
         }
         secs = secsId.value;
-        if(secs == null){
+        if(secs == null || secs < 0){
             secs = 0;
         }
         startThisUP=false;
@@ -46,12 +61,20 @@ function stopT() {
     paused = true;
 }
 var zeroedOut = false;
+function addZero(num){
+    if(num < 10){
+        return "0"+num.toString();
+    }else{
+        return num;
+    }
+}
 
 function timer() {
     if (!paused) {
         hoursId.disabled=true;
         minsId.disabled=true;
         secsId.disabled=true;
+        
         let c = document.getElementById("timerL");
      
         if (secs < 0) {
@@ -73,11 +96,19 @@ function timer() {
             hours = 0;
             mins=0;
         }
-        c.innerHTML = hours.toString() +":"+mins.toString()+":"+ secs.toString()
+        if(hours == 0 && mins == 0 && secs == 0){
+            // showNotification();
+            paused = true;
+        }
+
+        c.innerHTML = addZero(hours) +":"+addZero(mins)+":"+ addZero(secs);
         secs--;
 
     } else {
+        hoursId.disabled=false;
+        minsId.disabled=false;
+        secsId.disabled=false;
         return;
     }
 }
-var id = setInterval(timer, 100);
+var id = setInterval(timer, 1000);
