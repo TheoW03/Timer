c3db2f0a28442ee68de3c54d434ed39a06b0d03c
Theo <95878514+TheoW03@users.noreply.github.com>
2022-04-02 15:25:31 -0400

    added timer

--- Timer.js
+++ Timer.js
@@ -1,4 +1,4 @@
- // import {timerr} from "./TimerClass.js";
+// import {timerr} from "./TimerClass.js";
 //  function showNotification() {
 //     const notification = new Notification("New message incoming", {
 //        body: "your timer is up!!!"    
@@ -37,23 +37,23 @@ function getCalc() {
 
 }
 function startTimers() {
-    if(startThisUP){
+    if (startThisUP) {
         hoursId = document.getElementById("hours");
         minsId = document.getElementById("mins");
         secsId = document.getElementById("seconds");
         hours = hoursId.value;
-        if(hours == null || hours < 0){
+        if (hours == null || hours < 0) {
             hours = 0;
         }
         mins = minsId.value;
-        if(mins == null || mins < 0){
+        if (mins == null || mins < 0) {
             mins = 0;
         }
         secs = secsId.value;
-        if(secs == null || secs < 0){
+        if (secs == null || secs < 0) {
             secs = 0;
         }
-        startThisUP=false;
+        startThisUP = false;
     }
     paused = false;
 }
@@ -61,53 +61,56 @@ function stopT() {
     paused = true;
 }
 var zeroedOut = false;
-function addZero(num){
-    if(num < 10){
-        return "0"+num.toString();
-    }else{
+function addZero(num) {
+    if (num < 10) {
+        return "0" + num.toString();
+    } else {
         return num.toString();
     }
 }
 
 function timer() {
     if (!paused) {
-        hoursId.disabled=true;
-        minsId.disabled=true;
-        secsId.disabled=true;
-        
+        hoursId.disabled = true;
+        minsId.disabled = true;
+        secsId.disabled = true;
+
         let c = document.getElementById("timerL");
-     
+
         if (secs < 0) {
             secs = 59;
             mins--;
-            if(mins < 0){
-                secs=0;
+            if (mins < 0) {
+                secs = 0;
             }
-            
+
         }
-        if(mins < 0){
+        if (mins < 0) {
             hours--;
-            mins=59;
-            if(hours < 0){
-                mins=0;
+            mins = 59;
+            if (hours < 0) {
+                mins = 0;
             }
         }
-        if(hours < 0){
+        if (hours < 0) {
             hours = 0;
-            mins=0;
+            mins = 0;
         }
-        if(hours == 0 && mins == 0 && secs == 0){
+        if (hours == 0 && mins == 0 && secs == 0) {
             // showNotification();
+            const soundEffect = new Audio();
+            soundEffect.src = 'timerUp.mp3';
+            soundEffect.play();
             paused = true;
         }
 
-        c.innerHTML = addZero(hours) +":"+addZero(mins)+":"+ addZero(secs);
+        c.innerHTML = addZero(hours) + ":" + addZero(mins) + ":" + addZero(secs);
         secs--;
 
     } else {
-        hoursId.disabled=false;
-        minsId.disabled=false;
-        secsId.disabled=false;
+        hoursId.disabled = false;
+        minsId.disabled = false;
+        secsId.disabled = false;
         return;
     }
 }
