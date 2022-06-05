030a0c215e05019b365f1ee5ab7b7b04cdf07a9d
Theo <95878514+TheoW03@users.noreply.github.com>
2022-06-05 19:57:40 -0400

    E

--- Timer.js
+++ Timer.js
@@ -15,8 +15,8 @@
 //     showNotification();
 
 // }
-window.onload = function(){
-    document.getElementById("stopSound").hidden=true;
+window.onload = function () {
+    document.getElementById("stopSound").hidden = true;
 }
 var paused = true;
 var secs;
@@ -26,6 +26,7 @@ var minsId;
 var secsId;
 var hoursId;
 var startThisUP = true;
+var soundExists = false;
 function inputHours() {
 
 }
@@ -40,7 +41,7 @@ function getCalc() {
 
 }
 function startTimers() {
-    if (startThisUP) {
+    // if (startThisUP) {
         hoursId = document.getElementById("hours");
         minsId = document.getElementById("mins");
         secsId = document.getElementById("seconds");
@@ -48,19 +49,39 @@ function startTimers() {
         if (hours == null || hours < 0) {
             hours = 0;
         }
+        
         mins = minsId.value;
         if (mins == null || mins < 0) {
             mins = 0;
+        }
+        if(mins > 60){
+            // mins = mins%60;
+            // mins = parseInt(hours+ Math.floor(mins/60));
+            // alert(typeof(mins))
+            // mins = mins%60;
         }
         secs = secsId.value;
         if (secs == null || secs < 0) {
             secs = 0;
         }
+        if(secs > 60){
+            mins = parseInt(mins);
+            secs = parseInt(secs);
+            mins = parseInt(Math.floor(secs/60));
+            secs = parseInt(secs%60);
+        }
         startThisUP = false;
-    }
+    // }else{
+        // hours=hours;
+        // mins=mins;
+        // secs=secs;
+    // }
     paused = false;
 }
 function stopT() {
+    hoursId.value= hours;
+    minsId.value = mins;
+    secsId.value = secs;
     paused = true;
 }
 var zeroedOut = false;
@@ -104,13 +125,17 @@ function timer() {
             const soundEffect = new Audio();
             soundEffect.src = 'timerUp.mp3';
             // soundEffect.play();
-            document.getElementById("stopSound").hidden=false;
+            document.getElementById("stopSound").hidden = false;
             paused = true;
+            soundExists = true;
         }
 
         c.innerHTML = addZero(hours) + ":" + addZero(mins) + ":" + addZero(secs);
         secs--;
 
+
+    } else if (soundExists) {
+        return;
     } else {
         hoursId.disabled = false;
         minsId.disabled = false;
@@ -118,8 +143,12 @@ function timer() {
         return;
     }
 }
-function stopSound(){
-    document.getElementById("stopSound").hidden=true;
+function stopSound() {
+    document.getElementById("stopSound").hidden = true;
+    soundExists=false;
+    hoursId.disabled = false;
+    minsId.disabled = false;
+    secsId.disabled = false;
 
 }
 var id = setInterval(timer, 1000);
