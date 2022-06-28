91f496d473fb889f75d0528f14ed5f8b7ec1fdc8
Theo <95878514+TheoW03@users.noreply.github.com>
2022-06-28 13:55:58 -0400

    added server

--- app.js
+++ app.js
@@ -8,15 +8,23 @@ const app = express();
 const hostname = '127.0.0.1'
 const port = 3000
 
+//this creates local server
+
 app.use(express.static('Frontend'));
-app.use('/css',express.static(__dirname + 'Frontend/css'));
-app.use('/js',express.static(__dirname + 'Frontend/js'));
-app.use('/img',express.static(__dirname + 'Frontend/SocialIcon/img'));
-app.get('',(req,res) => {
+app.use('/css', express.static(__dirname + 'Frontend/css'));
+app.use('/js', express.static(__dirname + 'Frontend/js'));
+app.use('/img', express.static(__dirname + 'Frontend/SocialIcon/img'));
+app.use('/mp3', express.static(__dirname + 'Frontend/mp3'));
+app.get('', (req, res) => {
     res.sendFile(__dirname + 'Frontend/index.html');
     res.sendFile(__dirname + 'Frontend/stopwatch.html');
     res.sendFile(__dirname + 'Frontend/TimerPage.html');
     res.sendFile(__dirname + 'Frontend/WorldClock.html');
-    
+
 })
-app.listen(port, () => console.log(`https://localhost:3000/`));
+
+app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+
